fc0a529320222f1e38678bdf2dfcc8a98d580253
Italo Pires <italonapalm@gmail.com>
2020-10-17 23:08:15 -0300

    added validates to proponent model

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,2 +1,4 @@
 class Proponent < ApplicationRecord
+  validates :name, :cpf, :birthdate, :address_number, :neighborhood, :public_place, :cep,
+    :personal_phone, :salary, :inss_discount, presence: true
 end
