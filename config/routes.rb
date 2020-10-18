d4836bbd9e306e0b1c6ac370c36c214f45bc95cb
Italo Pires <italonapalm@gmail.com>
2020-10-17 23:00:49 -0300

    improvements on layout and added the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
   scope module: :web do
     resources :proponents, except: :show
     get 'inss_calculator', controller: :inss_calculators, action: :calculate
+
+    root to: 'proponents#index'
   end
 end
