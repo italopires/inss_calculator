50dafc4c564ea601b9a15ab8e3d527b0542dd172
Italo Pires <italonapalm@gmail.com>
2020-10-18 23:16:35 -0300

    improvement on js to load cities

--- app/assets/javascripts/web/proponents.js
+++ app/assets/javascripts/web/proponents.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   $('#proponent_state_id').on('change', function () {
     var state_id = $(this).val();
-    $.ajax({
-      type: 'GET',
-      url: `/api/v1/states/${state_id}/cities/`,
-      dataType: 'json',
-      success:function(data) {
-        city_field = $('#proponent_city_id')
-        city_field.html('');
+    if (state_id != '') {
+      $.ajax({
+        type: 'GET',
+        url: `/api/v1/states/${state_id}/cities/`,
+        dataType: 'json',
+        success: function (data) {
+          city_field = $('#proponent_city_id')
+          city_field.html('');
 
-        $.each(data.cities, function() {
-          city_field.append("<option value='"+this.id+"'>"+this.name+"</option>");
-        });
-      }
-    });
+          $.each(data.cities, function () {
+            city_field.append("<option value='" + this.id + "'>" + this.name + "</option>");
+          });
+        }
+      });
+    }
   });
 
   $("#proponent_salary").change(function(){
